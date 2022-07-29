d7effd1c16f9fc1fbb3a133b93e277b65597847b
thapadeepa05 <thapadeepa05@gmail.com>
2022-07-29 13:26:08 -0500

    Initial commit

---
+++ app/src/main/java/com/example/recipeapp/Models/Recipe.java
@@ -0,0 +1,40 @@
+package com.example.recipeapp.Models;
+
+import java.util.ArrayList;
+
+public class Recipe {
+    public boolean vegetarian;
+    public boolean vegan;
+    public boolean glutenFree;
+    public boolean dairyFree;
+    public boolean veryHealthy;
+    public boolean cheap;
+    public boolean veryPopular;
+    public boolean sustainable;
+    public int weightWatcherSmartPoints;
+    public String gaps;
+    public boolean lowFodmap;
+    public int aggregateLikes;
+    public double spoonacularScore;
+    public double healthScore;
+    public double pricePerServing;
+    public ArrayList<ExtendedIngredient> extendedIngredients;
+    public int id;
+    public String title;
+    public int readyInMinutes;
+    public int servings;
+    public String sourceUrl;
+    public String image;
+    public String imageType;
+    public String summary;
+    public ArrayList<Object> cuisines;
+    public ArrayList<String> dishTypes;
+    public ArrayList<String> diets;
+    public ArrayList<String> occasions;
+    public String instructions;
+    public ArrayList<AnalyzedInstruction> analyzedInstructions;
+    public Object sourceName;
+    public Object creditsText;
+    public Object originalId;
+    public String spoonacularSourceUrl;
+}
